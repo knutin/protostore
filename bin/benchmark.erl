fab55f675481587250fa211a05571d29d81145ed
Knut Nesheim <knutin@gmail.com>
2017-04-23 14:46:31 +0100

    Multiplexing client.

--- bin/benchmark.erl
+++ bin/benchmark.erl
@@ -1,23 +1,24 @@
 #!/usr/bin/env escript
 %% Localhost
-%% bin/benchmark.erl protostore 127.0.0.1 12345 2 2 /mnt/data/protostore.toc
-%% bin/benchmark.erl redis 127.0.0.1 6379 2 2 /mnt/data/protostore.toc
+%% bin/benchmark.erl protostore 127.0.0.1 12345 2 2 10 /mnt/data/protostore.toc
+%% bin/benchmark.erl redis 127.0.0.1 6379 2 2 10 /mnt/data/protostore.toc
 %%
 %% i3
-%% bin/benchmark.erl protostore 172.18.230.186 12345 2 2 /mnt/data/protostore.toc
+%% bin/benchmark.erl protostore 172.18.246.107 12345 2 2 10 /mnt/data/protostore.toc
 %%
-%% r3
-%% bin/benchmark.erl redis 172.18.229.112 6379 2 2 redis.toc
+%% r4
+%% bin/benchmark.erl redis 172.18.246.29 6379 2 2 10 redis.toc
 
 -mode(compile).
 
 -include_lib("kernel/include/file.hrl").
 
-main([ServerTypeString, Host, PortString, NumProcsString, RuntimeString, TocFile]) ->
+main([ServerTypeString, Host, PortString, NumProcsString, RuntimeString, MaxInflightString, TocFile]) ->
     ServerType = list_to_atom(ServerTypeString),
     Port = list_to_integer(PortString),
     NumProcs = list_to_integer(NumProcsString),
     Runtime = list_to_integer(RuntimeString),
+    MaxInflight = list_to_integer(MaxInflightString),
 
     %% Read uuids so we know which keys to fetch.
     Toc = read_toc(TocFile),
@@ -26,11 +27,12 @@ main([ServerTypeString, Host, PortString, NumProcsString, RuntimeString, TocFile
 
     io:format("Starting ~p clients~n", [NumProcs]),
     Pids = lists:map(fun (_) ->
-                             Sock = case gen_tcp:connect(Host, Port, [binary, {active, false}]) of
-                                        {ok, S} -> S;
-                                        _ -> throw(could_not_connect)
-                                    end,
-                             spawn_link(fun () -> hammer(ServerType, Sock, Toc) end)
+                             spawn_link(fun () ->
+                                                Sock = case gen_tcp:connect(Host, Port, [binary, {active, true}]) of
+                                                           {ok, S} -> S;
+                                                           _ -> throw(could_not_connect)
+                                                       end,
+                                                hammer(ServerType, Sock, MaxInflight, Toc) end)
                      end,
                      lists:seq(1, NumProcs)),
 
@@ -77,53 +79,48 @@ recv_all(T, L, [Pid | Pids]) ->
     end.
 
 
-hammer(ServerType, Sock, Toc) ->
+hammer(ServerType, Sock, MaxInflight, Toc) ->
     Position = trunc(rand:uniform() * (byte_size(Toc) div 20)),
     MaxPos = byte_size(Toc) div 20,
     ReqId = 0,
+    InflightReqs = #{},
 
     receive hammertime -> ok end,
     io:format("Process ~p: It's hammertime!~n", [self()]),
-    hammer(ServerType, Sock, ReqId, Toc, Position, MaxPos, [], []).
+    hammer(ServerType, Sock, ReqId, InflightReqs, MaxInflight, Toc, Position, MaxPos, [], []).
 
-hammer(ServerType, Sock, ReqId, Toc, MaxPos, MaxPos, Timings, Lens) ->
-    hammer(ServerType, Sock, ReqId, Toc, 0, MaxPos, Timings, Lens);
-hammer(protostore, Sock, ReqId, Toc, Pos, MaxPos, Timings, Lens) ->
-    Uuid = binary:part(Toc, Pos*20, 16),
 
-    %% Time the full roundtrip, including sending the request and
-    %% receiveng the full response.
-    {ElapsedUs, Result} = timer:tc(
-                            fun () ->
-                                    ok = gen_tcp:send(Sock, <<ReqId:32/unsigned-integer, Uuid/binary>>),
+hammer(ServerType, Sock, ReqId, InflightReqs, MaxInflight, Toc, MaxPos, MaxPos, Timings, Lens) ->
+    %% Wrap position around, start at 0 again
+    hammer(ServerType, Sock, ReqId, InflightReqs, MaxInflight, Toc, 0, MaxPos, Timings, Lens);
 
-                                    case gen_tcp:recv(Sock, 8, 5000) of
-                                        {ok, <<ReqId:32/unsigned-integer,  0:32/unsigned-integer>>} ->
-                                            throw(bad_response_length);
-                                        {ok, <<ReqId:32/unsigned-integer,  Len:32/unsigned-integer>>} ->
-                                            {ok, _Res} = gen_tcp:recv(Sock, Len, 5000),
-                                            {ok, Len};
-                                        {error, timeout} ->
-                                            io:format("Process ~p: timeout waiting for req id ~p, uuid ~p~n", [self(), ReqId, Uuid]),
-                                            throw(timeout);
-                                        Error ->
-                                            Error
-                                    end
-                            end),
+hammer(protostore, Sock, ReqId, InflightReqs, MaxInflight, Toc, Pos, MaxPos, Timings, Lens) ->
+
     receive
         {halt, Pid} ->
+            {_, NewTimings, NewLens} = recv(Sock, InflightReqs, Timings, Lens),
             ok = gen_tcp:close(Sock),
-            Pid ! {self(), results, Timings, Lens}
+            Pid ! {self(), results, NewTimings, NewLens}
     after 0 ->
-            case Result of
-                {ok, Len} ->
-                    hammer(protostore, Sock, ReqId+1, Toc, Pos+1, MaxPos, [ElapsedUs | Timings], [Len | Lens]);
-                Error ->
-                    throw({hammer_error, ReqId, Error})
+            case maps:size(InflightReqs) =:= MaxInflight of
+                true ->
+                    {NewInflightReqs, NewTimings, NewLens} = recv(Sock, InflightReqs, Timings, Lens),
+                    hammer(protostore, Sock, ReqId, NewInflightReqs, MaxInflight, Toc, Pos+1, MaxPos, NewTimings, NewLens);
+
+                false ->
+                    Uuid = binary:part(Toc, Pos*20, 16),
+                    Now = erlang:convert_time_unit(erlang:system_time(), native, microsecond),
+
+                    ok = gen_tcp:send(Sock, <<ReqId:32/unsigned-integer, Uuid/binary>>),
+                    NewInflightReqs = maps:put(ReqId, Now, InflightReqs),
+
+                    hammer(protostore, Sock, ReqId+1, NewInflightReqs, MaxInflight, Toc, Pos+1, MaxPos, Timings, Lens)
             end
     end;
 
-hammer(redis, Sock, ReqId, Toc, Pos, MaxPos, Timings, Lens) ->
+
+
+hammer(redis, Sock, ReqId, InflightReqs, MaxInflightReqs, Toc, Pos, MaxPos, Timings, Lens) ->
     Uuid = binary:part(Toc, Pos*20, 16),
 
     %% Time the full roundtrip, including sending the request and
@@ -164,11 +161,55 @@ hammer(redis, Sock, ReqId, Toc, Pos, MaxPos, Timings, Lens) ->
     after 0 ->
             case Result of
                 {ok, Len} ->
-                    hammer(redis, Sock, ReqId+1, Toc, Pos+1, MaxPos, [ElapsedUs | Timings], [Len | Lens]);
+                    hammer(redis, Sock, ReqId+1, InflightReqs, MaxInflightReqs, Toc, Pos+1, MaxPos, [ElapsedUs | Timings], [Len | Lens]);
                 Error ->
                     throw({hammer_error, ReqId, Error})
             end
     end.
+
+
+
+%% Receive as many responses are available without blocking
+recv(Sock, Inflight, Timings, Lens) ->
+    recv(Sock, <<>>, Inflight, Timings, Lens).
+
+
+recv(Sock, <<>>, Inflight, Timings, Lens) ->
+    receive
+        {tcp, Sock, Data} ->
+            parse(Sock, Data, Inflight, Timings, Lens)
+    after 0 ->
+            {Inflight, Timings, Lens}
+    end;
+
+recv(Sock, Partial, Inflight, Timings, Lens) ->
+    receive
+        {tcp, Sock, Data} ->
+            parse(Sock, <<Partial/binary, Data/binary>>, Inflight, Timings, Lens)
+    end.
+
+parse(Sock, Data, Inflight, Timings, Lens) ->
+    case Data of
+        <<_:32/unsigned-integer,  0:32/unsigned-integer, _/binary>> ->
+            throw(bad_response);
+
+        <<ReqId:32/unsigned-integer,  Len:32/unsigned-integer, Body/binary>> ->
+            case byte_size(Body) >= Len of
+                true ->
+                    <<_ResponseBody:Len/binary, Rest/binary>> = Body,
+
+                    End = erlang:convert_time_unit(erlang:system_time(), native, microsecond),
+                    {Start, NewInflight} = maps:take(ReqId, Inflight),
+                    ElapsedUs = End - Start,
+                    parse(Sock, Rest, NewInflight, [ElapsedUs | Timings], [Len | Lens]);
+                false ->
+                    recv(Sock, Data, Inflight, Timings, Lens)
+            end;
+        _ ->
+            recv(Sock, Data, Inflight, Timings, Lens)
+    end.
+
+
 
 
 to_bulk(B) when is_binary(B) ->
